69dca2f9ff53c66b6bd3ca9c44a25a729e68c571
Nathan Archer <pheianox@gmail.com>
2021-02-08 01:42:25 +0400

    Init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const DIGITS = "1234567890";
+const LOWER_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+const UPPER_CASE_LETTERS = LOWER_CASE_LETTERS.toUpperCase();
+const SPECIAL_CHARACTERS = " |!\"#$%&'^_`*+-~.,:;=<>?@()[]{}/\\";
+
