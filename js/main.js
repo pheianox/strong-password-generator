60133fdaf9325aa9838bd8630ed95bf9dcb40530
Nathan Archer <77569421+pheianox@users.noreply.github.com>
2021-02-10 16:08:51 +0400

    Quick refactor

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 "use strict"
-
 import generator from "./generator.js";
 
 const config = {
-    lowerCase: 0, upperCase: 0, digits: 0, specialCharacters: 0, length: 8
-}
+    lowerCase: 0, 
+    upperCase: 0, 
+    digits: 0, 
+    specialCharacters: 0, 
+    length: 8
+};
+
 generator()
